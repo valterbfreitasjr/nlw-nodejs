ec263ed522cab56a54423964c3cf233196071272
Valter B Freitas Jr <valter191@hotmail.com>
2024-07-13 21:36:39 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/teste", () => {
+  return "Hello World";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running.");
+});
