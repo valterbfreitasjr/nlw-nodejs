a9d1e51a0d42590a678c80dd5d64b8517f92ee01
Valter B Freitas Jr <valter191@hotmail.com>
2024-08-03 22:00:47 -0300

    feat: add create-link endpoint

---
+++ nlw-nodejs/src/routes/create-link.ts
@@ -0,0 +1,45 @@
+import type { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function createLink(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips/:tripId/links",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          title: z.string(),
+          url: z.string().url(),
+        }),
+      },
+    },
+    async (req) => {
+      const { tripId } = req.params;
+      const { title, url } = req.body;
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+
+      const link = await prisma.link.create({
+        data: {
+          title,
+          url,
+          trip_id: tripId,
+        },
+      });
+
+      return { linkId: link.id };
+    }
+  );
+}
