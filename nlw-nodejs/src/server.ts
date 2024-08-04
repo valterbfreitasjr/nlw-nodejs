ee0ff805b7b4b1ada413f848cf2b0ff3378ca544
Valter B Freitas Jr <valter191@hotmail.com>
2024-08-03 22:02:00 -0300

    feat: add new routes on server

--- nlw-nodejs/src/server.ts
+++ nlw-nodejs/src/server.ts
@@ -9,6 +9,10 @@ import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipant } from "./routes/confirm-participants";
 import { createActivities } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-link";
+import { getParticipants } from "./routes/get-participant";
+import { createInvite } from "./routes/create-invite";
 
 const app = fastify();
 
@@ -25,6 +29,10 @@ app.register(confirmTrip);
 app.register(confirmParticipant);
 app.register(createActivities);
 app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running.");
