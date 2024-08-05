57954fdd450acac0cf936b0f485dca8910551f6e
Valter B Freitas Jr <valter191@hotmail.com>
2024-08-04 23:09:03 -0300

    feat: add create-activity endpoint

--- nlw-nodejs/src/routes/create-activity.ts
+++ nlw-nodejs/src/routes/create-activity.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import dayjs from "dayjs";
+import { ClientError } from "../error/client-error";
 
 export async function createActivities(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -29,15 +30,15 @@ export async function createActivities(app: FastifyInstance) {
       });
 
       if (!trip) {
-        throw new Error("Trip not found");
+        throw new ClientError("Trip not found");
       }
 
       if (dayjs(occurs_at).isBefore(trip.starts_at)) {
-        throw new Error("Invalid activity date - occurst before starts.");
+        throw new ClientError("Invalid activity date - occurst before starts.");
       }
 
       if (dayjs(occurs_at).isAfter(trip.ends_at)) {
-        throw new Error("Invalid activity date - occurs after the end.");
+        throw new ClientError("Invalid activity date - occurs after the end.");
       }
 
       const activity = await prisma.activity.create({
