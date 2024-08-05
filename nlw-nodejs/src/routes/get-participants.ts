3f0afe66a517012c66f0b3be9c8ca552bccb79a4
Valter B Freitas Jr <valter191@hotmail.com>
2024-08-04 23:07:41 -0300

    feat: add get-participants endpoint

---
+++ nlw-nodejs/src/routes/get-participants.ts
@@ -0,0 +1,44 @@
+import type { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { ClientError } from "../error/client-error";
+
+export async function getParticipants(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/participants",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+    async (req) => {
+      const { tripId } = req.params;
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+        include: {
+          participants: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              is_confirmed: true,
+              is_owner: true,
+            },
+          },
+        },
+      });
+
+      if (!trip) {
+        throw new ClientError("Trip not found");
+      }
+
+      return { participants: trip.participants };
+    }
+  );
+}
